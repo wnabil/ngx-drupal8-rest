8ef8461f5f582275e5d6e13c901c44d88d865833
Wassem Keddah <wassemkeddah@gmail.com>
2018-11-26 13:47:15 +0200

    angualr 7

---
+++ projects/ngx-drupal8-rest/src/public_api.ts
@@ -0,0 +1,8 @@
+/*
+ * Public API Surface of ngx-drupal8-rest
+ */
+
+export * from './lib/config';
+export * from './lib/models';
+export * from './lib/http';
+export * from './lib/ngx-drupal8-rest.module';
